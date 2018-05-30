023366404e8aa8e966ad62b012741ca4815e9cfb
bitansaha <bitansaha.1@gmail.com>
2018-05-29 23:39:51 -0500

    Api to get Test data

--- app/controllers/blaster_controller.go
+++ app/controllers/blaster_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import "github.com/revel/revel"
 import "github.com/bitansaha/blaster/app/blaster/services"
+import "github.com/bitansaha/blaster/app/blaster/networkerrors"
 
 type BlasterController struct {
 	*revel.Controller
@@ -13,4 +14,23 @@ func (blasterController BlasterController) GetTests() revel.Result {
 	} else {
 		return blasterController.RenderJSON(tests)
 	}
+}
+
+func (blasterController BlasterController) GetTest(testName string) revel.Result {
+	if test, err := services.GetTestByName(testName); err != nil {
+		switch err.(type) {
+		case *networkerrors.Error500:
+			return blasterController.RenderError(err)
+		case *networkerrors.Error404:
+			return blasterController.NotFound(err.Error())
+		default:
+			response := blasterController.Response
+			response.SetStatus(500)
+			response.GetWriter().Write([]byte("Unknown Error: " + err.Error()))
+			return blasterController.Result
+		}
+	} else {
+		return blasterController.RenderJSON(test)
+	}
+
 }
