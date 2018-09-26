244bf9e0dd85e6fa5cfb1a5ea038a5763273b0cc
bitansaha <bitansaha.1@gmail.com>
2018-09-25 22:30:39 -0500

    Adding the Play button

--- web-client/src/js/common/button/CustomButton.js
+++ web-client/src/js/common/button/CustomButton.js
@@ -6,23 +6,30 @@ class CustomButton extends Component {
   constructor(props){
     super(props);
 
-    const _buttonStyle = (props.buttonStyle ? props.buttonStyle : "primary")
+    this.getButtonStyle = this.getButtonStyle.bind(this);
+    this.isButtonEnabled = this.isButtonEnabled.bind(this);
+  }
 
-    this.state = {
-      buttonName: props.buttonName,
-      buttonFunction: props.buttonFunction,
-      buttonStyle: _buttonStyle
-    }
+  getButtonStyle() {
+    return (this.props.buttonStyle ? this.props.buttonStyle : "primary");
+  }
 
-    this.state.buttonFunction = this.state.buttonFunction.bind(this);
+  isButtonEnabled() {
+    return (this.props.enabled ? this.props.enabled : true);
   }
 
   render() {
     return (
       <div>
-        <Button onClick={this.state.buttonFunction} bsStyle={this.state.buttonStyle} block>
-          {this.state.buttonName}
-        </Button>
+        {this.isButtonEnabled() ?
+          (<Button onClick={this.props.buttonFunction} bsStyle={this.getButtonStyle()} block>
+            {this.props.buttonName}
+          </Button>)
+          :
+          (<Button onClick={this.props.buttonFunction} bsStyle={this.getButtonStyle()} block disabled>
+            {this.props.buttonName}
+          </Button>)
+        }
       </div>
     );
   }
--- web-client/src/js/view/test/MainComponent.js
+++ web-client/src/js/view/test/MainComponent.js
@@ -9,8 +9,13 @@ class MainComponent extends Component {
   constructor(props) {
       super(props);
       this.handleOnSelect = this.handleOnSelect.bind(this);
+      this.testDefinationUpdate = this.testDefinationUpdate.bind(this);
+      this.saveData = this.saveData.bind(this);
+      this.run = this.run.bind(this);
       this.state = {
         isSelected: true,
+        isSave: false,
+        isRun: false,
         testDefinationData: {
           testName: "",
           numberOfUser: 0,
@@ -27,17 +32,47 @@ class MainComponent extends Component {
       this.setState({isSelected: !this.state.isSelected});
   }
 
+  testDefinationUpdate(testDefinationData) {
+    if (!testDefinationData.testName || 0 === testDefinationData.testName.length) {
+      this.setState({
+        isSave: false,
+        testDefinationData: testDefinationData
+      });
+    } else {
+      this.setState({
+        isSave: true,
+        isRun: false,
+        testDefinationData: testDefinationData
+      });
+    }
+
+  }
+
+  saveData() {
+    alert("Saving Data")
+    // TODO: Verify the overall state of all Data before marking isRun as true
+    this.setState({
+      isSave: false,
+      isRun: true
+    });
+  }
+
+  run() {
+    alert("Running")
+  }
+
   render() {
     return (
       <div>
         <Row>
           <Col sm={1}></Col>
           <Col sm={3}>
-              <TestPanel handleOnSelect={this.handleOnSelect} isSelected={this.state.isSelected}/>
+              <TestPanel handleOnSelect={this.handleOnSelect} isSelected={this.state.isSelected}
+              isSave={this.state.isSave} saveData={this.saveData} isRun={this.state.isRun} run={this.run}/>
           </Col>
           <Col sm={7}>
             {this.state.isSelected ? (
-              <TestDefinitionForm testDefinationData={this.state.testDefinationData}/>
+              <TestDefinitionForm testDefinationData={this.state.testDefinationData} testDefinationUpdate={this.testDefinationUpdate}/>
             ) : (
               <ExecutionPlan executionPlanData={this.state.executionPlanData}/>
             )}
